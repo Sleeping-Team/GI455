2b7a26509f04141ba87d0c5f667739cfca85c4f0
Thanayos Siratranont <tuaa1bb2@gmail.com>
2023-02-21 20:15:08 +0700

    Change Interaction Method
    from Spherecast to Collider

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -37,7 +37,7 @@ public class PlayerInteraction : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other == null)
+        if (_interactingObject == null)
         {
             _interactingObject = other.gameObject;
         }
@@ -45,7 +45,7 @@ public class PlayerInteraction : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other != null)
+        if (_interactingObject == other.gameObject)
         {
             _interactingObject = null;
         }
