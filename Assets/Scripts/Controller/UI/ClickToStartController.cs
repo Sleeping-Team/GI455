17193b615e88e5b3b8476b5141996207319bbeba
Thaleesa <97783447+Thaleesa@users.noreply.github.com>
2023-02-27 11:47:18 +0700

    UI Adjustment
    create main menu and change Ui when click to start

---
+++ Assets/Scripts/Controller/UI/ClickToStartController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickToStartController : MonoBehaviour
+{
+	[SerializeField] private Button closeButton;
+	[SerializeField] private GameObject firstScene;
+	[SerializeField] private GameObject nameScene;
+	
+	void Start () 
+	{
+		Button btn = closeButton.GetComponent<Button>();
+		btn.onClick.AddListener(ButtonOnClick);
+	}
+
+	void ButtonOnClick()
+	{
+		Debug.Log ("You have clicked the button!");
+		firstScene.SetActive(false);
+		nameScene.SetActive(true);
+	}
+    
+}
