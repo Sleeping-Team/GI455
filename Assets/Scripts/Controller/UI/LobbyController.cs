6eb3c613900721221d4edf660ab00137eea5350c
DHMEsport <tkunopag@gmail.com>
2023-05-03 21:43:02 +0700

    is now play maybe

--- Assets/Scripts/Controller/UI/LobbyController.cs
+++ Assets/Scripts/Controller/UI/LobbyController.cs
@@ -11,11 +11,8 @@ public class LobbyController : MonoBehaviour
     
     [SerializeField] private Button continueButton;
     [SerializeField] private Button exitButton;
-    [SerializeField] private Button confirmButton;
-    [SerializeField] private Button NotconfirmButton;
-    
+
     [SerializeField] private GameObject pauseUI;
-    [SerializeField] private GameObject confirmGameObjectl;
     
     // Start is called before the first frame update
     void Start()
@@ -30,12 +27,9 @@ public class LobbyController : MonoBehaviour
         }
 
         Button continueBtn = continueButton.GetComponent<Button>();
-        Button confirmBtn = confirmButton.GetComponent<Button>();
         Button exitBtn = exitButton.GetComponent<Button>();
-        Button NotconfirmBtn = NotconfirmButton.GetComponent<Button>();
+        
         continueBtn.onClick.AddListener(ContinueButtonOnClick);
-        confirmBtn.onClick.AddListener(confirmButtomOnClick);
-        NotconfirmBtn.onClick.AddListener(NoconfirmButtomOnClick);
         exitBtn.onClick.AddListener(ExitButtonOnClick);
         
         //pauseUI.SetActive(false);
@@ -67,36 +61,20 @@ public class LobbyController : MonoBehaviour
     {
         pauseUI.SetActive(true);
     }
-
-    public void confirmButtom()
-    {
-        confirmGameObjectl.SetActive(true);
-    }
+    
     public void ContinueButtonOnClick()
     {
         //check the button is clicked
         Debug.Log("You have clicked the continue button!");
         
         pauseUI.SetActive(false);
-        confirmGameObjectl.SetActive(false);
-    }
-
-    public void confirmButtomOnClick()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-
-    public void NoconfirmButtomOnClick()
-    {
-        confirmGameObjectl.SetActive(false);
-        pauseUI.SetActive(true);
     }
     
     public void ExitButtonOnClick()
     {
         //check the button is clicked
         Debug.Log ("You have clicked the exit button!");
-        pauseUI.SetActive(false);
-        confirmGameObjectl.SetActive(true);
+
+        SceneManager.LoadScene("MainMenu");
     }
 }
